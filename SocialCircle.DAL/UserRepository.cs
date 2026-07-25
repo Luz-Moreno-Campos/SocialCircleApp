a2813200d5957edb3fe4d27f9125cfe510966067
Luz Moreno Campos <luzdirection@gmail.com>
2026-07-25 15:05:36 -0500

    created repository methods to fecth data

--- SocialCircle.DAL/UserRepository.cs
+++ SocialCircle.DAL/UserRepository.cs
@@ -23,6 +23,46 @@ namespace SocialCircle.DAL
                 .FirstOrDefault(u => u.UserName == username && u.Password == password);
         }
 
+        public User GetUserById(int userId)
+        {
+            return _context.Users
+                .FirstOrDefault(u => u.UserId == userId);
+        }
+
+        public List<Post> GetUserPosts(int userId)
+        {
+            return _context.Posts
+                .Where(p => p.UserId == userId)
+                .OrderByDescending(p => p.PostTimeStamp)
+                .ToList();
+        }
+
+        public List<User> GetFollowers(long userId)
+        {
+            var followerIds = _context.Follows
+                .Where(f => f.FollowingId == userId)
+                .Select(f => f.FollowerId)
+                .ToList();
+
+            return _context.Users
+                .Where(u => followerIds.Contains(u.UserId))
+                .ToList();
+        }
+
+        public List<User> GetFollowing(long userId)
+        {
+            var followingIds = _context.Follows
+                .Where(f => f.FollowerId == userId)
+                .Select(f => f.FollowingId)
+                .ToList();
+
+            return _context.Users
+                .Where(u => followingIds.Contains(u.UserId))
+                .ToList();
+        }
+
+
+
     }
 
 }
---
+++ SocialCircle.MVC/Models/UserViewModel.cs
@@ -0,0 +1,14 @@
+﻿using SocialCircle.Models;
+
+namespace SocialCircle.MVC.Models
+{
+    public class UserViewModel
+    {
+
+        public User User { get; set; }
+        public List<Post> Posts { get; set; }
+        public List<User> Followers { get; set; }
+        public List<User> Following { get; set; }
+    }
+}
+}
