cae8678a3116b467a0c8d07e619925f1edbdef80
roop3754 <roopjeetkaur23@gmail.com>
2026-07-26 16:47:43 -0500

    Add post comment file in controller

---
+++ SocialCircle.MVC/Controllers/PostController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocialCircle.MVC.Controllers
+{
+    public class PostController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
