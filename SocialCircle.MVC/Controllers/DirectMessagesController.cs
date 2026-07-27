004997a63c77c05c96f08b82ff317ee6b94f0875
Manvir kaur <manvirkaur8193@gmail.com>
2026-07-27 10:33:15 -0500

    intial commit

---
+++ SocialCircle.MVC/Controllers/DirectMessagesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocialCircle.MVC.Controllers
+{
+    public class DirectMessagesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SocialCircle.MVC/Controllers/LikesController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocialCircle.MVC.Controllers
+{
+    public class LikesController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
