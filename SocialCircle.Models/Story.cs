3f187e09ccd876aeb92b3a6d100cdb6dd956afcb
Ferguson Zibiri <excelzibiri5@gmail.com>
2026-07-27 21:53:26 -0500

    rando

--- SocialCircle.Models/Story.cs
+++ SocialCircle.Models/Story.cs
@@ -20,5 +20,5 @@ public partial class Story
 
     public virtual User User { get; set; } = null!;
 
-    // public virtual ICollection<User> Users { get; set; } = new List<User>();
+    public virtual ICollection<User> Users { get; set; } = new List<User>();
 }
